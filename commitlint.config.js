ebf3fe70b1bcff57e6e168c1b1ae41c96daf67de
xiexujie <xiexujie@bytedance.com>
2022-04-28 21:19:09 +0800

    feat: remove byted host

--- commitlint.config.js
+++ commitlint.config.js
@@ -17,8 +17,6 @@ module.exports = {
     ],
     "type-case": [0],
     "type-empty": [2, "never"],
-    "scope-empty": [2, "never"],
-    "scope-case": [0],
     "subject-full-stop": [0, "never"],
     "subject-case": [0, "never"],
     "header-max-length": [0, "always", 72],
