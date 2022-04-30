e3359c8efcd32eee37605062f286f1d83dd08d18
郑南欣 <zhengnanxin@bytedance.com>
2022-04-30 23:07:49 +0800

    test eslint

--- packages/blog-webpack5/src/main.js
+++ packages/blog-webpack5/src/main.js
@@ -2,4 +2,4 @@ import { createApp } from "vue";
 import App from "./App.vue"; // 测试文件 App.vue 和 用于挂载的index.html 自行创建
 import "windi.css";
 
-createApp(App).mount("#app");
+ createApp(App).mount("#app");
