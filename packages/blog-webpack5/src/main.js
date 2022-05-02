dfc26a59c5d22e8edd77c16e702ef8b249300d85
xiexujie <xiexujie@bytedance.com>
2022-05-02 11:39:35 +0800

    feat: add sentry in blog-webpack5

--- packages/blog-webpack5/src/main.js
+++ packages/blog-webpack5/src/main.js
@@ -1,5 +1,28 @@
 import { createApp } from "vue";
-import App from "./App.vue"; // 测试文件 App.vue 和 用于挂载的index.html 自行创建
+import * as Sentry from "@sentry/vue";
+import { BrowserTracing } from "@sentry/tracing";
+import { createRouter } from "vue-router";
+import App from "./App.vue";
 import "windi.css";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+// const router = createRouter({
+//   // TODO...
+// });
+
+// Inspire by Sentry: https://docs.sentry.io/platforms/javascript/
+Sentry.init({
+  app,
+  dsn: "https://bd05332124b54823857f37e39dfe8eb2@o1228307.ingest.sentry.io/6374015",
+  integrations: [
+    new BrowserTracing({
+      // routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      tracingOrigins: ["localhost", "my-site-url.com", /^\//],
+    }),
+  ],
+  tracesSampleRate: 1.0,
+  debug: true,
+  logErrors: true,
+});
+
+app.mount("#app");
