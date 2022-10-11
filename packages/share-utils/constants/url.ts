1fb9c362f62e8e0ed364f08c00a44e464d32669e
郑南欣 <zhengnanxin@bytedance.com>
2022-10-11 18:14:53 +0800

    feat: host change

--- packages/share-utils/constants/url.ts
+++ packages/share-utils/constants/url.ts
@@ -1,2 +1,3 @@
 // export const host = "http://192.168.2.11:4000";
-export const host = 'http://localhost:4000';
+// export const host = 'http://localhost:4000';
+export const host = 'http://124.220.189.226:4000';
